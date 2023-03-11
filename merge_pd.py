487f7747758f748bb46fa6df6c077b7025cc58dc
Daniel Waldram <waldram.daniel@gmail.com>
2023-03-11 11:43:12 -0700

    adding descriptive header to merged_pd

--- merge_pd.py
+++ merge_pd.py
@@ -1,3 +1,8 @@
+"""
+This script is used to evaluate the team_transform function, which transforms team names from the march_madness_85-21.csv
+dataset. The script prints the number of teamnames that mismatched between the two databases and prints a list of these teams
+to mismatched_teams.csv.
+"""
 import pandas as pd
 from support_functions import *
 
@@ -5,14 +10,16 @@ def main():
     # read in the full dataset
     results = pd.read_csv('march_madness_85-21.csv')
     stats = pd.read_csv('cbb_stats_webscrape.csv')
+    # Get the min and max years over which data needs to be matched. These are the years of data that will
+    #   be used to train data, so we need the team names to match.
+    min_year = min([results['YEAR'].max(), stats['YEAR'].max()])
+    max_year = max([results['YEAR'].min(), stats['YEAR'].min()])
 
     # grab the same years from each: stats: 2013-2019, results: 1985-2021
-    stats = stats[2007 < stats['YEAR']][2023 > stats['YEAR']].sort_values(['YEAR', 'TEAM'])
-    results = results[(2007 < results['YEAR']) & (2023 > results['YEAR'])].sort_values(['YEAR', 'A_TEAM'])
+    stats = stats[min_year <= stats['YEAR']][max_year >= stats['YEAR']].sort_values(['YEAR', 'TEAM'])
+    results = results[(min_year <= results['YEAR']) & (max_year >= results['YEAR'])].sort_values(['YEAR', 'A_TEAM'])
     # replace all instances of State in results with St.
     results['A_TEAM'] = results['A_TEAM'].apply(teamname_transform)
-
-    # transform names in the results to match with stats using teamname conversion
 
     # Grab the unique values from the team column for each
     stats_unique = stats['TEAM'].unique()
@@ -23,7 +30,7 @@ def main():
 
     # find any remaining mismatched data and print save to csv
     mismatched_results_df = pd.DataFrame(data=mismatched_results, columns=['TEAM'])
-    print(len(mismatched_results_df))
+    print(f'There are {len(mismatched_results_df)} mismatched team names')
     mismatched_results_df.to_csv('mismatched_teams.csv', index=False)
 
 
